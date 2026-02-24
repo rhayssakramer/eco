cbc1b7fc0a3ef16776018add04a45816b0376e57
rhayssakramer <rhayssakramer@hotmail.com>
2026-02-23 21:47:02 -0300

    feat: adicionar entidade Denuncia e configuração DbSet

---
+++ Eco.Api/Models/Denuncia.cs
@@ -0,0 +1,15 @@
+using Eco.Api.Enums;
+
+namespace Eco.Api.Models;
+
+public class Denuncia
+{
+    public int Id { get; set; }
+    public TipoDenuncia Tipo { get; set; }
+    public string? Descricao { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
+    public StatusDenuncia Status { get; set; } = StatusDenuncia.Recebido;
+    public double? Latitude { get; set; }
+    public double? Longitude {get; set; }
+
+}
